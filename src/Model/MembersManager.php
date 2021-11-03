77a32334374dee58e33191f5cb0bac620ee0ab06
Sayn <marvincrepin@gmail.com>
2021-11-03 19:10:19 +0100

    Members Manager : Created Members Manager

---
+++ src/Model/MembersManager.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Model;
+
+class MembersManager extends AbstractManager
+{
+    public const TABLE = 'members';
+
+    /**
+     * Insert new member in database
+     */
+    public function insert(array $member): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`,) VALUES (:name)");
+        $statement->bindValue('name', $member['name'], \PDO::PARAM_STR);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
+}
