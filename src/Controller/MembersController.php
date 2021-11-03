2bfa21312f9c570329d15c64f610dcac36e906d2
Sayn <marvincrepin@gmail.com>
2021-11-03 20:36:53 +0100

    Added security to form

--- src/Controller/MembersController.php
+++ src/Controller/MembersController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Model\MembersManager;
+use Exception;
 
 class MembersController extends AbstractController
 {
@@ -19,11 +20,15 @@ class MembersController extends AbstractController
             $member = array_map('trim', $_POST);
 
             // TODO validations (length, format...)
-
+            if(!empty($member['name'])){
+                $membersManager = new MembersManager();
+                $membersManager->insert($member);
+                header('Location: /');
+            }
+            else {
+                throw new Exception('Tu dois entrer un nom !');
+            }
             // if validation is ok, insert and redirection
-            $membersManager = new MembersManager();
-            $membersManager->insert($member);
-            header('Location: /');
         }
         return $this->twig->render('Home/index.html.twig', ['members' => $members]);
     }
